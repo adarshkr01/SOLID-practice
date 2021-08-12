573b6a669cc2a47c0b0276e6af2f387ee5bb125d
Niekon01 <adarsh90909090@gmail.com>
2021-08-12 12:25:16 +0530

    Initial Commit - BankApplication

---
+++ BankApplication/BankApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
