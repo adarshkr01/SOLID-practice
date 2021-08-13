a10e5c43b7060ee759735bcdc743426aee1d9c9a
Niekon01 <adarsh90909090@gmail.com>
2021-08-13 11:22:27 +0530

    ExpenseTracker init

---
+++ ExpenseTracker/ExpenseTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpenseTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
